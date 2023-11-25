43963b0c38d6630771c3f4999e42a2e2b97fc89c
Vladik288 <55274156+Vladik288@users.noreply.github.com>
2023-11-25 17:33:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Employee(BaseModel):
+    first_name: str
+    last_name: str
+    company: str
+    age: int
+    phone: Optional[str] = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello():
+    return {"Hello": "API"}
+
+
+@app.post("/create_employee/")
+def create_employee(employee: Employee):
+    return {
+        "status": "created",
+        "data": employee
+    }
